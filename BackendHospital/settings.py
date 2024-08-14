bb253c672415ca62531affc1f736340035ef3606
cesarbmathec <prodeprogramacion@gmail.com>
2024-08-14 00:05:33 -0400

    Version 3

--- BackendHospital/settings.py
+++ BackendHospital/settings.py
@@ -177,7 +177,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "cesarbmath@gmail.com"
-EMAIL_HOST_PASSWORD = "uxdp qdhh vbfu jniv"
+EMAIL_HOST_PASSWORD = ""
 DEFAULT_FROM_EMAIL = "no-reply@hospital.com"
 
 
